76646fcb210f60bae1e239cc631f4aefac444c98
Naufal Taufiq Ridwan <71002775+n0l3r@users.noreply.github.com>
2022-10-03 19:19:54 +0700

    Update promiseAsyncAwait.js

--- promiseAsyncAwait.js
+++ promiseAsyncAwait.js
@@ -2,7 +2,7 @@ async function getPosts() {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
     return data;
-    }
+}
 
 
 getPosts()
